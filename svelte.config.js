f2e04ee42ec344557b5e7b7362489e108ad1b03a
Jane Fan <jfan9988@gmail.com>
2024-04-07 19:56:44 +1000

    remove /dream-girl path used previously for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.env.NODE_ENV === 'development'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -10,10 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-    paths: {
-      base: dev ? '' : '/dream-girl',
-    },
     adapter: adapter({
+      runtime: 'nodejs20.x',
     })
 	}
 };
