4dc3cae863dd8c8322e39dee0a7fe3a7d707cd5d
Jane Fan <jfan9988@gmail.com>
2024-04-25 14:54:42 +1000

    add loading screen content

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { HTMLButtonAttributes } from 'svelte/elements';
+
+  export let props: HTMLButtonAttributes | undefined = undefined;
+</script>
+
+<button {...props} on:click>
+  <slot />
+</button>
+
+<style>
+  button {
+    font-size: 1.5rem;
+    color: var(--c-white);
+    background: transparent;
+    border: 1px solid var(--c-white);
+    border-radius: 2rem;
+    padding: 0.5rem 1rem;
+  }
+
+  button:hover {
+    background-color: var(--c-white-semi);
+    cursor: pointer;
+  }
+</style>
--- src/lib/components/loading-screen.svelte
+++ src/lib/components/loading-screen.svelte
@@ -1,22 +1,86 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { fade, scale, slide } from 'svelte/transition';
+  import Button from './button.svelte';
   /**
    * Loading progress value 0 - 100
    */
   export let progress = 0;
+
+  let showbgImage = false;
+  let hasEntered = false;
+  $: isReady = progress === 100;
+
+  onMount(() => {
+    showbgImage = true;
+  });
 </script>
 
-{#if progress < 100}
-  <div class="background">
-    <span>{progress.toFixed(0)}%</span>
+{#if !hasEntered}
+  <div class="background" transition:slide={{ axis: 'y', duration: 1000 }}>
+    {#if showbgImage}
+      <div
+        class="loading-screen-image"
+        transition:slide={{ axis: 'y', duration: 1000, delay: 1500 }}
+      />
+    {/if}
+  </div>
+
+  <div class="content" transition:scale={{ duration: 500 }}>
+    <img class="logo" src="/img/logo.png" alt="Dream girl logo" />
+    {#if isReady}
+      <div transition:fade>
+        <Button
+          on:click={() => {
+            console.log('here');
+            hasEntered = true;
+          }}>Enter</Button
+        >
+      </div>
+    {:else}
+      <span class="progress-amount">{progress.toFixed(0)}%</span>
+    {/if}
   </div>
 {/if}
 
 <style>
   .background {
-    background-color: var(--c-navy);
-    inset: 0;
     position: fixed;
+    inset: 0;
+
+    background-color: var(--c-navy);
+  }
+
+  .loading-screen-image {
+    position: absolute;
+    inset: 0;
+
+    background-image: url('/img/loading-screen.jpg');
+    background-position: center center;
+    background-size: cover;
+  }
+
+  .content {
+    position: fixed;
+    inset: 0;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .logo {
+    position: relative;
+    width: 500px;
+    max-width: 100%;
+    object-fit: contain;
+  }
+
+  .progress-amount {
+    position: relative;
+    font-size: 3rem;
     color: var(--c-white);
-    z-index: 1;
+    padding-bottom: 4rem;
   }
 </style>
